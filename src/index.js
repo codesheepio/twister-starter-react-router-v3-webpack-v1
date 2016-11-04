589f18cafd021f462220b69cbaed7b789ca96299
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-11-04 17:09:05 +0700

    Add NewTweet component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import customStyle from './styles/custom.scss'
 import mainStyle from './styles/main.scss'
-import TweetList from './components/TweetList'
+import NewTweet from './components/NewTweet'
 
-const tweets = [
-  {
-    id: 0, name: 'Supasate Choochaisri', username: 'kaizerwing',
-    tweetText: 'Lorem Ipsum ...' 
-  },
-  {
-    id: 1, name: 'Arnupharp Viratanapanu', username: 'topscores',
-    tweetText: 'Lorem Ipsum ...' 
-  },
-]
 const App = () => (
-  <TweetList tweets={tweets} />
+  <NewTweet />
 )
 ReactDOM.render(<App />, document.getElementById('react-root'))
