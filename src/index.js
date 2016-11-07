44d93e0f2983079ef023f65ad95d9588189ca7d1
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-08 01:48:58 +0700

    Add sample MainLayout render in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import customStyle from './styles/custom.scss'
 import mainStyle from './styles/main.scss'
+import MainLayout from './layouts/MainLayout'
 import BodyContainer from './components/BodyContainer'
 
 const App = () => (
-  <BodyContainer ownerUsername="kaizerwing" />
+  <MainLayout>
+    <BodyContainer ownerUsername="kaizerwing" />
+  </MainLayout>
 )
 ReactDOM.render(<App />, document.getElementById('react-root'))
