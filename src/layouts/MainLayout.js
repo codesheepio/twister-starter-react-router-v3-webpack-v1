cc01dc8876edfc4f4140f1347c5bd42b221401c4
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-08 01:42:52 +0700

    Add MainLayout

---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const MainLayout = (props) => (
+  <div>
+    <NavBar />
+    { props.children }
+  </div>
+)
+
+export default MainLayout
