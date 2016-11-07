ccd2a0abce3a190037ccd14b999bae90e419d5cd
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-07 21:30:47 +0700

    Render Profile in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import customStyle from './styles/custom.scss'
 import mainStyle from './styles/main.scss'
-import MainPanel from './components/MainPanel'
+import Profile from './components/Profile'
 
 const App = () => (
-  <MainPanel />
+  <Profile
+    name="Arnupharp Viratanapanu"
+    username="topscores"
+    numTweets={123}
+    numFollowers={50}
+    numFollowings={100}
+    isFollowing
+    isOwnProfile
+    toggleFollow={() => {console.log("toggle click")}}
+  />
 )
 ReactDOM.render(<App />, document.getElementById('react-root'))
