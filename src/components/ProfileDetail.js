208e7b01ce454969ae9015401ede8104e002da46
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-07 21:53:11 +0700

    Add uncommited files

---
+++ src/components/ProfileDetail.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ProfileDetail = ({ numTweets, numFollowers, numFollowings }) => {
+  return (
+    <div className="detail">
+      <div className="col">
+        <div className="text">Tweets</div>
+        <div className="number">{numTweets}</div>
+      </div>
+      <div className="col">
+        <div className="text">Followers</div>
+        <div className="number">{numFollowers}</div>
+      </div>
+      <div className="col">
+        <div className="text">Followings</div>
+        <div className="number">{numFollowings}</div>
+      </div>
+    </div>
+  )
+}
+
+ProfileDetail.propTypes = {
+  numTweets: React.PropTypes.number.isRequired,
+  numFollowers: React.PropTypes.number.isRequired,
+  numFollowings: React.PropTypes.number.isRequired,
+}
+
+export default ProfileDetail
---
+++ src/components/ProfileFollow.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import classnames from 'classnames'
+
+const ProfileFollow = (props) => {
+  const btnClass = classnames('btn btn-lg', {
+    'btn-danger': props.isFollowing,
+    'btn-default': !props.isFollowing,
+  })
+
+  const followToggleBtn = props.isFollowing ?
+    <input
+      type="button"
+      className={btnClass}
+      value="Unfollow"
+      onClick={props.handleToggleFollow}
+    />
+    :
+    <input
+      type="button"
+      className={btnClass}
+      value="Follow"
+      onClick={props.handleToggleFollow}
+    />
+
+  return (
+    <div className="action last-section">
+      { followToggleBtn }
+    </div>
+  )
+}
+
+
+ProfileFollow.propTypes = {
+  isFollowing: React.PropTypes.bool,
+  handleToggleFollow: React.PropTypes.func.isRequired,
+}
